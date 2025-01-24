041da8f387ca6efe49988c51e3707e3f89ae453f
bokigolic <bokigolic33@gmail.com>
2025-01-23 23:49:47 -0500

    Fetch api works

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,8 +1,40 @@
-const Weather = () => {
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Weather() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('https://api.open-meteo.com/v1/forecast', {
+        params: {
+          latitude: '52.52',  // Example latitude
+          longitude: '13.41', // Example longitude
+          current: 'temperature_2m,wind_speed_10m',
+          hourly: 'temperature_2m,relative_humidity_2m,wind_speed_10m'
+        }
+      });
+      setWeatherData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h2>Weather app</h2>
+      <h1>Weather Forecast</h1>
+      {weatherData ? (
+        <div>
+          <p>Current temperature: {weatherData.current.temperature_2m} °C</p>
+          <p>Wind speed: {weatherData.current.wind_speed_10m} km/h</p>
+          <p>Humidity: {weatherData.current.relative_humidity_2m}%</p>
+          <p>Cloud cover: {weatherData.current.cloud_cover_100m}%</p>
+          <p>Precipitation: {weatherData.current.precipitation_sum} mm</p>
+        </div>
+      ) : (
+        <p>Loading data...</p>
+      )}
     </div>
   );
-};
+}
 export default Weather;
